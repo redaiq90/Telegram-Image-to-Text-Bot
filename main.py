e5514f0c439f86e062d463d317d148eed977f1e4
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2024-06-19 01:51:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ async def ocr_image(update: Update, context: CallbackContext):
     # Download the photo
     photo_file = await update.message.photo[-1].get_file()
     photo_path = 'temp_photo.jpg'
-    await photo_file.download(photo_path)
+    await photo_file.download_to_drive(photo_path)
+
 
     try:
         # Ask user to choose language
