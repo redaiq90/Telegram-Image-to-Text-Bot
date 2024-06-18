2757b7e99dc4453bef3fc77f53ab5c9f99d0f3e6
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2024-06-19 02:50:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def help_command(update: Update, context: CallbackContext):
 
 async def ocr_image(update: Update, context: CallbackContext):
     # Download the photo
-    photo_file = update.message.photo[-1].get_file()
+    photo_file = await update.message.photo[-1].get_file()
     photo_path = 'temp_photo.jpg'
     await photo_file.download_to_drive(photo_path)
 
