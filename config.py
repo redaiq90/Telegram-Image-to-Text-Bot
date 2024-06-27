f59f0dff0c1d50979580346acddecdc09a1859ce
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2024-06-27 11:27:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-# Define your token and OCR API key here
+import os
 TOKEN = os.environ.get("TOKEN", 8080) #telegram token from botfather
 OCR_API_KEY = os.environ.get("OCR_API", 8080) #you can get it from https://ocr.space/ 
